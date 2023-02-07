3c588f8f135ebb6664cdfe9a2d0ee42bbb5cc70d
Joao Barbosa <joao@mail.com>
2023-02-07 00:10:38 -0300

    first commit

---
+++ app/src/main/java/com/example/cartaoaniversario/ResultadoActivity.kt
@@ -0,0 +1,48 @@
+package com.example.cartaoaniversario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "$ACTIVITY onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "$ACTIVITY onStart")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "$ACTIVITY onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "$ACTIVITY onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"$ACTIVITY onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"$ACTIVITY onDestroy")
+    }
+    companion object {
+        private const val TAG = "IGTILog"
+        private const val ACTIVITY = "ResultadoActivity::"
+    }
+}
